bbe180bcfe0c939e65ff7fbf0602cc66a427b19b
putrinada <putrinada1177@gmail.com>
2024-08-18 22:49:52 +0700

    Update

--- newman_script/putri-api.js
+++ newman_script/putri-api.js
@@ -1,5 +1,12 @@
+const newman = require("newman");
+
 newman.run({
     collection: "json-collection/putri.postman_collection.json",
-    environment: "json-env/putri-env.postman_environment.json",
-    reporters: ["cli", "htmlextra"]
+    environment: "json-env/putri-env.postman_environment.json",  
+    reporters: ["cli", "htmlextra"] 
+}, function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log("Collection run complete!");
 });
